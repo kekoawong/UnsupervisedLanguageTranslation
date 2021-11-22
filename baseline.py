a2852021860fa78fc29fbd6a7b6e5633c7763dc0
kekoawong <kwong6@nd.edu>
2021-11-21 19:21:36 -0500

    Got initial model to work

--- baseline.py
+++ baseline.py
@@ -24,23 +24,25 @@ for line in data:
     lines.append(listToStr)
 labels = [line[1] for line in data]
 
+# Encode labels
+encoder = LabelEncoder()
+labels = encoder.fit_transform(labels)
+
 # tfidf scores
 vectorizer = TfidfVectorizer(tokenizer=custom_tokenizer)
 tfidf = vectorizer.fit_transform(lines)
-
 # separate into training and testing
-xTrain, xTest, yTrain, yTest = train_test_split(tfidf, labels, test_size=0.2,random_state=20)
-print(xTrain.shape)
+xTrain, xTest, yTrain, yTest = train_test_split(tfidf, labels, test_size=0.2, random_state=20)
 
 #Calling the Class
 naive_bayes = GaussianNB()
  
 #Fitting the data to the classifier
-naive_bayes.fit(xTrain.todense() , yTrain)
+naive_bayes.fit(xTrain.toarray() , yTrain)
  
 #Predict on test data
-y_predicted = naive_bayes.predict(xTest)
-accuracy = metrics.accuracy_score(y_predicted , y_test)
+y_predicted = naive_bayes.predict(xTest.toarray())
+accuracy = metrics.accuracy_score(y_predicted , yTest)
 
 
 print("Classifier accuracy percent:", accuracy)
