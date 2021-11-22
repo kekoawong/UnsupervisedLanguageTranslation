864212979a4fe964432fd217400c5d039c80495e
kekoawong <kwong6@nd.edu>
2021-11-22 00:20:14 -0500

    Fixed initial baseline

--- baselineNegation.py
+++ baselineNegation.py
@@ -1,5 +1,6 @@
 import pickle
 import nltk
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
 # get data
 dataFile = 'data/sentences.pickle'
@@ -10,16 +11,30 @@ with open(dataFile, 'rb') as dFile:
 lines = []
 for line in data:
     sent = line[0]
-    for word,pos in nltk.pos_tag(nltk.word_tokenize(str(sent))):
+    tokens = nltk.word_tokenize(str(sent))
+    for word,pos in nltk.pos_tag(tokens):
         if (pos.startswith("J")):
-            ind = sent.find(word)
-            sent1 = sent[:ind]
-            sent2 = sent[ind:]
-            sent = sent1 + "not " + sent2
+            ind = tokens.index(word)
+            tokens.insert(ind, "not")
+            sent = TreebankWordDetokenizer().detokenize(tokens)
             break
         elif (pos.startswith("V")):
-            ind = sent.find(word)
-            sent1 = sent[:ind]
-            sent2 = sent[ind:]
-            sent = sent1 + "not " + sent2
+            ind = tokens.index(word) + 1
+            tokens.insert(ind, "not")
+            sent = TreebankWordDetokenizer().detokenize(tokens)
             break
+    
+    if line[0] == sent:
+        print(f'Not able to translate: {sent}')
+        print()
+    else:
+        if int(line[1]) == 1:
+            print('IRONIC --> UNIRONIC')
+            print(f'{line[0]}')
+            print(f'{sent}')
+            print()
+        elif int(line[1]) == -1:
+            print('UNIRONIC --> IRONIC')
+            print(f'{line[0]}')
+            print(f'{sent}')
+            print()
