a379706172601287811154745bfa2a36c308ccc9
kekoawong <kwong6@nd.edu>
2021-12-10 15:39:38 -0500

    Added unsupervised training

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Unsupervised Machine Translation System without parallel text
 Using RNNs. 
 
 ## Methods
-Using an unsuperived machine learning approach, translate ironic statements to ironic statements
+Using an unsuperived machine learning approach, translate ironic statements to ironic statements  
 
-Works as follows:
-Foreign --> Target
+Works as follows:  
+Foreign --> Target  
 
-Use a rough translation system at first (using baselineNegation.py)
-Then, using this rough translation system, initialize two systems
-Pass the output data of each system into the input of the next system.
-System 1: Foreign --> Target
-System 2: Target --> Foreign
-No parallel text. 
+Use a rough translation system at first (using baselineNegation.py)  
+Then, using this rough translation system, initialize two systems  
+Pass the output data of each system into the input of the next system.  
+System 1: Foreign --> Target  
+System 2: Target --> Foreign  
+No parallel text.  
---
+++ dualSystem.py
@@ -0,0 +1,97 @@
+import torch
+from translationModel import *
+
+if __name__ == "__main__":
+    import argparse, sys
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train', type=str, help='training data')
+    parser.add_argument('--dev', type=str, help='development data')
+    parser.add_argument('infile', nargs='?', type=str, help='test data to translate')
+    parser.add_argument('-o', '--outfile', type=str, help='write translations to file')
+    parser.add_argument('--load', type=str, help='load model from file')
+    parser.add_argument('--save', type=str, help='save model in file')
+    args = parser.parse_args()
+
+    if args.train:
+        # Read training data and create vocabularies
+        traindata = read_parallel(args.train)
+
+        fvocab = Vocab()
+        evocab = Vocab()
+        for fwords, ewords in traindata:
+            fvocab |= fwords
+            evocab |= ewords
+
+        # Create model
+        m = Model(fvocab, 64, evocab) # try increasing 64 to 128 or 256
+        
+        if args.dev is None:
+            print('error: --dev is required', file=sys.stderr)
+            sys.exit()
+        devdata = read_parallel(args.dev)
+            
+    elif args.load:
+        if args.save:
+            print('error: --save can only be used with --train', file=sys.stderr)
+            sys.exit()
+        if args.dev:
+            print('error: --dev can only be used with --train', file=sys.stderr)
+            sys.exit()
+        m = torch.load(args.load)
+
+    else:
+        print('error: either --train or --load is required', file=sys.stderr)
+        sys.exit()
+
+    if args.infile and not args.outfile:
+        print('error: -o is required', file=sys.stderr)
+        sys.exit()
+
+    if args.train:
+        opt = torch.optim.Adam(m.parameters(), lr=0.0003)
+
+        best_dev_loss = None
+        for epoch in range(10):
+            random.shuffle(traindata)
+
+            ### Update model on train
+
+            train_loss = 0.
+            train_ewords = 0
+            for fwords, ewords in progress(traindata):
+                loss = -m.logprob(fwords, ewords)
+                opt.zero_grad()
+                loss.backward()
+                opt.step()
+                train_loss += loss.item()
+                train_ewords += len(ewords) # includes EOS
+
+            ### Validate on dev set and print out a few translations
+            
+            dev_loss = 0.
+            dev_ewords = 0
+            for line_num, (fwords, ewords) in enumerate(devdata):
+                dev_loss -= m.logprob(fwords, ewords).item()
+                dev_ewords += len(ewords) # includes EOS
+                if line_num < 10:
+                    translation = m.translate(fwords)
+                    print(' '.join(translation))
+
+            if best_dev_loss is None or dev_loss < best_dev_loss:
+                best_model = copy.deepcopy(m)
+                if args.save:
+                    torch.save(m, args.save)
+                best_dev_loss = dev_loss
+
+            print(f'[{epoch+1}] train_loss={train_loss} train_ppl={math.exp(train_loss/train_ewords)} dev_ppl={math.exp(dev_loss/dev_ewords)}', flush=True)
+            
+        m = best_model
+
+    ### Translate test set
+
+    if args.infile:
+        with open(args.outfile, 'w') as outfile:
+            for fwords in read_mono(args.infile):
+                translation = m.translate(fwords)
+                print(' '.join(translation), file=outfile)
