7f0c254758f1d5e08afef0f85844ec739c7acf34
kekoawong <kwong6@nd.edu>
2021-12-10 17:25:25 -0500

    Initial traction

--- translationModel.py
+++ translationModel.py
@@ -240,7 +240,7 @@ if __name__ == "__main__":
 
     if args.train:
         # Read training data and create vocabularies
-        traindata = read_parallel(args.train)
+        traindata = read_mono(args.train)
 
         fvocab = Vocab()
         evocab = Vocab()
