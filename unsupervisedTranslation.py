c07d201669440f80e7a4f97377e3f15c3ef4ee21
kekoawong <kwong6@nd.edu>
2021-12-11 13:10:50 -0500

    Added argument parsing

--- unsupervisedTranslation.py
+++ unsupervisedTranslation.py
@@ -1,48 +1,48 @@
 import torch
 from translationModel import *
+from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
     import argparse, sys
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--trainf', type=str, help='foreign training data')
-    parser.add_argument('--traint', type=str, help='target training data')
-    parser.add_argument('--dev', type=str, help='development data')
-    parser.add_argument('infile', nargs='?', type=str, help='test data to translate')
-    parser.add_argument('-o', '--outfile', type=str, help='write translations to file')
+    parser.add_argument('--dataf', type=str, help='foreign language data')
+    parser.add_argument('--datat', type=str, help='target language data')
+    parser.add_argument('--initial', 'infile', dest='initial', type=str, help='Initial rough translation of foreign language data into target language')
+    parser.add_argument('--percentTrain', type=str, help='Percent to be used for training data, the remaining will be test')
+    parser.add_argument('--epochs', '-e', dest='epochs', type=str, help='Number of epochs to train per model per iteration')
+    parser.add_argument('-iterations', '-i', dest='iterations', type=str, help='Number of epochs to train per model per iteration')
+    parser.add_argument('-o', '--outfile', dest='iterations', type=str, help='write translations to file')
     parser.add_argument('--load', type=str, help='load model from file')
     parser.add_argument('--save', type=str, help='save model in file')
     args = parser.parse_args()
 
-    if args.train:
-        # Read training data and create vocabularies
-        traindata = read_mono(args.train)
+    if args.dataf and args.initial and args.datat:
+
+        # Read in data
+        dataf = read_mono(args.dataf)
+        datat = read_mono(args.dataf)
+        initialTranslation = read_mono(args.initial)
+
+        # split training and testing data
+        percentTrain = 0.95 if not args.percentTrain else float(args.percentTrain)
+        
 
         fvocab = Vocab()
-        evocab = Vocab()
-        for fwords, ewords in traindata:
+        tvocab = Vocab()
+        initialTransVocab = Vocab()
+        for fwords in dataf:
             fvocab |= fwords
-            evocab |= ewords
+        for twords in datat:
+            tvocab |= twords
+        for roughWords in initialTranslation:
+            initialTransVocab |= roughWords
 
-        # Create model
-        m = Model(fvocab, 64, evocab) # try increasing 64 to 128 or 256
-        
-        if args.dev is None:
-            print('error: --dev is required', file=sys.stderr)
-            sys.exit()
-        devdata = read_parallel(args.dev)
-            
-    elif args.load:
-        if args.save:
-            print('error: --save can only be used with --train', file=sys.stderr)
-            sys.exit()
-        if args.dev:
-            print('error: --dev can only be used with --train', file=sys.stderr)
-            sys.exit()
-        m = torch.load(args.load)
+        # Create initial translation model
+        target_to_foreign = Model(fvocab, 64, tvocab) # try increasing 64 to 128 or 256
 
     else:
-        print('error: either --train or --load is required', file=sys.stderr)
+        print('error: foreign data, target data, and rough initial translation all required', file=sys.stderr)
         sys.exit()
 
     if args.infile and not args.outfile:
@@ -57,7 +57,6 @@ if __name__ == "__main__":
             random.shuffle(traindata)
 
             ### Update model on train
-
             train_loss = 0.
             train_ewords = 0
             for fwords, ewords in progress(traindata):
