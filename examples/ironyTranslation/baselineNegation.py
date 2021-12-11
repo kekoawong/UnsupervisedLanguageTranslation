c9eb35bfb6d1e81a5d43ab0170652da53cabba0e
kekoawong <kwong6@nd.edu>
2021-12-11 14:53:52 -0500

    ALtered script to function

--- examples/ironyTranslation/baselineNegation.py
+++ examples/ironyTranslation/baselineNegation.py
@@ -2,15 +2,7 @@ import pickle
 import nltk
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 
-# get data
-dataFile = 'data/sentences.pickle'
-
-with open(dataFile, 'rb') as dFile:
-    data = pickle.load(dFile)
-
-lines = []
-for line in data:
-    sent = line[0]
+def negateSentence(sent):
     tokens = nltk.word_tokenize(str(sent))
     for word,pos in nltk.pos_tag(tokens):
         if (pos.startswith("J")):
@@ -23,18 +15,30 @@ for line in data:
             tokens.insert(ind, "not")
             sent = TreebankWordDetokenizer().detokenize(tokens)
             break
-    
-    if line[0] == sent:
-        print(f'Not able to translate: {sent}')
-        print()
-    else:
-        if int(line[1]) == 1:
-            print('IRONIC --> UNIRONIC')
-            print(f'{line[0]}')
-            print(f'{sent}')
+    return sent
+
+if __name__ == "__main__":
+    # get data
+    dataFile = 'data/sentences.pickle'
+
+    with open(dataFile, 'rb') as dFile:
+        data = pickle.load(dFile)
+
+    lines = []
+    for line in data:
+        sent = negateSentence(line[0])
+        
+        if line[0] == sent:
+            print(f'Not able to translate: {sent}')
             print()
-        elif int(line[1]) == -1:
-            print('UNIRONIC --> IRONIC')
-            print(f'{line[0]}')
-            print(f'{sent}')
-            print()
+        else:
+            if int(line[1]) == 1:
+                print('IRONIC --> UNIRONIC')
+                print(f'{line[0]}')
+                print(f'{sent}')
+                print()
+            elif int(line[1]) == -1:
+                print('UNIRONIC --> IRONIC')
+                print(f'{line[0]}')
+                print(f'{sent}')
+                print()
